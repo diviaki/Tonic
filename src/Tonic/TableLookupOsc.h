d9b323bd875e96f5addebb38979bbf3fa670cbd5
Akos <timcook@Tim-MacBook-Pro.local>
2016-07-27 15:03:20 +0200

    TableLookupOsc lookupTable_ access is not really needed, removed

--- src/Tonic/TableLookupOsc.h
+++ src/Tonic/TableLookupOsc.h
@@ -60,10 +60,6 @@ namespace Tonic {
       //! set sample table for lookup. MUST BE POWER OF 2 IN LENGTH
       void setLookupTable( SampleTable table );
 
-      SampleTable getLookupTable(){
-        return lookupTable_;
-      }
-
       void setPhase( float p ){
         phase_ = p;
       }
